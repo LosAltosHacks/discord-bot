10c88c09df6a2f774bb69abb6be2a9614dc9eb0f
Akash Vig <49599791+aksh1@users.noreply.github.com>
2021-04-22 18:54:56 -0700

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args, extras) => {
-    return message.channel.send("Hi there! I am the **Los Altos Hacks Bot** and I'm a Discord bot made specifically for the Los Altos Hacks Discord. I'm also open-source! You can view my code at <https://github.com/LosAltosHacks/discord-bot>. To view my commands, do `h!help`.");
+    return message.channel.send("Hi there! I am the **Los Altos Hacks Bot** and I'm a Discord bot made specifically for the Los Altos Hacks Discord. I'm also open-source! You can view my code at <https://github.com/LosAltosHacks/discord-bot>. To view my commands, do `!help`.");
 }
 
 module.exports.config = {
