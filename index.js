827ce9c36acf884d1ee962e4575901f181b39a2b
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-09-24 02:35:44 -0700

    Use new discord.js classes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { guildId, token } = require('./config.json');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+// const { clientId, clientSecret, guildId, token } = require('./config.json');
+// const axios = require('axios');
+// const { URLSearchParams } = require('url');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Registering commands
 client.commands = new Collection();
@@ -17,42 +20,71 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+// Get Client Credentials/Bearer Token to register bot command permissions
+// Add this line to config.json "clientSecret": "fGiqfbyrEr2LscUCtD0U2o6RUgxfwPnH",
+// const options = {
+//   method: 'POST',
+//   url: 'https://discord.com/api/v10/oauth2/token',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//   },
+//   data: new URLSearchParams({
+//     grant_type: 'client_credentials',
+//     scope: 'identify connections',
+//   }),
+//   auth: {
+//     username: clientId,
+//     password: clientSecret,
+//   },
+// };
+
+// const bearerToken = axios(options)
+//   .then(function (response) {
+//     return response.access_token;
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//     throw new Error(error);
+//   });
+
 // Ready-check
 client.once('ready', async () => {
   console.log('Ready!');
 
-  const guild = client.guilds.cache.get(guildId);
-  await guild.commands.fetch().then((commands) =>
-    commands.each((command) => {
-      const prohibitedRoles = client.commands.get(command.name).prohibitedRoles;
-      const permittedRoles = client.commands.get(command.name).permittedRoles;
-      if (!prohibitedRoles && !permittedRoles) return;
+  // const guild = client.guilds.cache.get(guildId);
+  // await guild.commands.fetch().then((commands) =>
+  //   commands.each((command) => {
+  //     const prohibitedRoles = client.commands.get(command.name).prohibitedRoles;
+  //     const permittedRoles = client.commands.get(command.name).permittedRoles;
+  //     if (!prohibitedRoles && !permittedRoles) return;
 
-      const prohibitedPermissions = prohibitedRoles
-        ? prohibitedRoles.map((roleId) => {
-            return {
-              id: roleId,
-              type: 'ROLE',
-              permission: false,
-            };
-          })
-        : [];
-      const permittedPermissions = permittedRoles
-        ? permittedRoles.map((roleId) => {
-            return {
-              id: roleId,
-              type: 'ROLE',
-              permission: true,
-            };
-          })
-        : [];
-      client.application.commands.permissions.set({
-        guild: guildId,
-        command: command.id,
-        permissions: [...prohibitedPermissions, ...permittedPermissions],
-      });
-    }),
-  );
+  //     const prohibitedPermissions = prohibitedRoles
+  //       ? prohibitedRoles.map((roleId) => {
+  //           return {
+  //             id: roleId,
+  //             type: 'ROLE',
+  //             permission: false,
+  //           };
+  //         })
+  //       : [];
+  //     const permittedPermissions = permittedRoles
+  //       ? permittedRoles.map((roleId) => {
+  //           return {
+  //             id: roleId,
+  //             type: 'ROLE',
+  //             permission: true,
+  //           };
+  //         })
+  //       : [];
+  //     client.application.commands.permissions.set({
+  //       token: bearerToken,
+  //       guild: guildId,
+  //       command: command.id,
+  //       permissions: [...prohibitedPermissions, ...permittedPermissions],
+  //     });
+  //   }),
+  // );
 });
 
 // Slash command handling
